ff40554b1f75c8b1bb6764bbd5a13c61d55cc36f
ciaaaa22 <azkia.zain@mhs.politala.ac.id>
2025-09-16 23:51:13 +0800

    oke

--- Mahasiswa/routes/web.php
+++ Mahasiswa/routes/web.php
@@ -3,10 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MahasiswaController;
 
-// route halaman utama
+// Halaman utama
 Route::get('/', function () {
     return view('welcome');
 });
 
-// route resource untuk mahasiswa (CRUD)
+// Route resource untuk Mahasiswa
 Route::resource('mahasiswa', MahasiswaController::class);
