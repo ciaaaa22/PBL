ca8bd634a866ca9dc0055b956efc3b08ee97692d
ciaaaa22 <azkia.zain@mhs.politala.ac.id>
2025-09-16 21:17:57 +0800

    ini database

---
+++ Mahasiswa/app/Models/Mahasiswa.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mahasiswa extends Model
+{
+    use HasFactory;
+
+    protected $table = 'mahasiswas';
+
+    protected $fillable = [
+        'nim',
+        'nama_mahasiswa',
+        'email',
+        'password',
+        'angkatan',
+        'role',
+        'id_ketua',
+        'id_kelompok',
+        'nama',
+    ];
+}
