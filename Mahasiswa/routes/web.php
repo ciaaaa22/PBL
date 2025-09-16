4bd99cde640910dfa3c73fdea6f5fd0da8491f00
devipusparina19 <devi.pusparina@mhs.politala.ac.id>
2025-09-16 22:32:03 +0700

    delete

--- Mahasiswa/routes/web.php
+++ Mahasiswa/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
 
+// route halaman utama
 Route::get('/', function () {
     return view('welcome');
 });
+
+// route resource untuk mahasiswa (CRUD)
+Route::resource('mahasiswa', MahasiswaController::class);
